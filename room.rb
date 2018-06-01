c703f9055207e9c586ecc305cc0e9562e72e0553
jamos90 <jamos_98@hotmail.com>
2018-06-01 14:48:00 +0100

    adding customers to rooms error

--- room.rb
+++ room.rb
@@ -2,12 +2,34 @@ class Room
 
    attr_reader :capacity, :till_ammount, :room_cost
 
-  def initialize(capacity, till_ammount, room_cost, track_list)
+  def initialize(capacity, till_ammount, room_cost, track_list, max_occupancy)
+
    @capacity = capacity
    @till_ammount = till_ammount
    @room_cost = room_cost
+   @track_list = track_list
+   @max_occupancy = max_occupancy
+ end
+
+ def track_list_length
+   return @track_list.length()
+ end
+
+ def add_song(song)
+   return @track_list.push(song)
+ end
+
+ def customer_count
+   return @capacity.length()
+ end
+
+ def add_customer_to_room(customer)
+   return if @capacity.count >= @max_occupancy
+  return @cpacity.push(customer)
+ end
 
 
-  end
+
+
 
 end
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -8,15 +8,37 @@ class TestRoom < MiniTest::Test
   def setup
    @song1 = Song.new("Cost Line")
    @song2 = Song.new("Total Ecplipse of the Heat")
-   track_list = [@song1, @song2]
+   @song3 = Song.new("Sweet Child of Mine")
+   @track_list = [@song1, @song2]
+   @customer1 = Customer.new("James", 50, "Coast Line")
+   @customer2 = Customer.new("Jess", 100, "Total Eclipse of the Heart")
+   @customer3 = Customer.new("Kate", 200, "Sweet Child Of Mine")
+   @capacity = [@customer1, @customer2]
 
-    @room1 = Room.new(12, 150, 50, track_list)
+    @room1 = Room.new(@capacity,150, 50, @track_list, 5)
   end
 
   def test_room_attribures
-    assert_equal(12, @room1.capacity)
     assert_equal(150, @room1.till_ammount)
     assert_equal(50, @room1.room_cost)
+  end
+
+  def test_track_list_length
+    assert_equal(2, @room1.track_list_length)
+  end
+
+  def test_add_songs_to_rooms
+    @room1.add_song(@song3)
+    assert_equal(3, @room1.track_list_length)
+  end
+
+  def test_occupancy_of_room
+    assert_equal(2, @room1.customer_count)
+  end
+
+  def test_add_customers_to_room
+    @room1.add_customer_to_room(@customer3)
+    assert_equal(3, @room1.customer_count)
   end
 
 end
